60094dbcb981b047836e1fc942d4eedf9d98d75a
XaviInsti <xavier.guillamon.7e5@itb.cat>
2023-02-13 20:54:39 +0100

    List done

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -15,40 +15,87 @@
  */
 package com.example.affirmations
 
+import android.content.pm.ApplicationInfo
 import android.os.Bundle
+import android.provider.ContactsContract.Data
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
 import com.example.affirmations.model.Affirmation
+import com.example.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      // TODO 5. Show screen
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AffirmationApp()
+        }
     }
-  }
 }
 
 @Composable
 fun AffirmationApp() {
-  // TODO 4. Apply Theme and affirmation list
+    AffirmationsTheme() {
+        AffirmationList(affirmationList = Datasource().loadAffirmations())
+    }
 }
 
 @Composable
 fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
-  // TODO 3. Wrap affirmation card in a lazy column
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(affirmationList.count()) { element ->
+            AffirmationCard(affirmation = affirmationList[element])
+            Spacer(modifier = Modifier.size(15.dp))
+        }
+    }
 }
 
 @Composable
 fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
-  // TODO 1. Your card UI
+    Card(
+        shape = RoundedCornerShape(15.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = stringResource(
+                    id = affirmation.stringResourceId
+                ),
+            )
+            Text(
+                text = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
 }
-
+/*
 @Preview
 @Composable
 private fun AffirmationCardPreview() {
-  // TODO 2. Preview your card
+    AffirmationList(affirmationList = Datasource().loadAffirmations())
 }
+*/
